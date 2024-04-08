8ccb7a669d2d0f4b7850e65b46e3ad56b4847345
Joaquin Niembro Bueno <v-jobueno@expediagroup.com>
2024-04-08 16:51:13 -0600

    user db methods

--- internal/data/models.go
+++ internal/data/models.go
@@ -18,16 +18,19 @@ type Models struct {
 		Delete(id int64) error
 		GetAll(title string, genres []string, filters Filters) ([]*Movie, Metadata, error)
 	}
+	Users UserModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
 		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
 	}
 }
 
 func NewMockModels() Models {
 	return Models{
 		Movies: MockMovieModel{},
+		Users:  UserModel{},
 	}
 }
