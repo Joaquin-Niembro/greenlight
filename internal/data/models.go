390189b47c0dd90d5ec56d326575be9efba06f0a
Joaquin Niembro Bueno <v-jobueno@expediagroup.com>
2024-04-17 10:15:57 -0600

    add permissions model to db and models in data package

--- internal/data/models.go
+++ internal/data/models.go
@@ -18,22 +18,25 @@ type Models struct {
 		Delete(id int64) error
 		GetAll(title string, genres []string, filters Filters) ([]*Movie, Metadata, error)
 	}
-	Users  UserModel
-	Tokens TokenModel
+	Users       UserModel
+	Tokens      TokenModel
+	Permissions PermissionModel
 }
 
 func NewModels(db *sql.DB) Models {
 	return Models{
-		Movies: MovieModel{DB: db},
-		Users:  UserModel{DB: db},
-		Tokens: TokenModel{DB: db},
+		Movies:      MovieModel{DB: db},
+		Users:       UserModel{DB: db},
+		Tokens:      TokenModel{DB: db},
+		Permissions: PermissionModel{DB: db},
 	}
 }
 
 func NewMockModels() Models {
 	return Models{
-		Movies: MockMovieModel{},
-		Users:  UserModel{},
-		Tokens: TokenModel{},
+		Movies:      MockMovieModel{},
+		Users:       UserModel{},
+		Tokens:      TokenModel{},
+		Permissions: PermissionModel{},
 	}
 }
